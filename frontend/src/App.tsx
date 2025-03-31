ecac7bd297c84874b51988fc4c8f7fc90de66e6c
Tanish Singhal <tanishsinghal510@gmail.com>
2025-03-31 12:59:14 +0530

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="font-semibold text-rose-500 bg-rose-200 p-2">
+      Hi everyone
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryGreen: "#a9fa4c",
+        primaryGreenHover: "#8cdb1c",
+        primary: "#bd93f9",
+        secondary: "#ff79c6",
+        error: "#ff5555",
+        textColor: "#d6d7db",
+        background: "#232530",
+        success: "#51fa7b",
+        card: "#30323e",
+        backgroundHighlight: "#4a4e61",
+        tag: "#f1fa8c",
+      },
+    },
+  },
+  plugins: [],
+};
